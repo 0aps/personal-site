4d80f98bfe0848b50eaf87791d614c9bf79ffc12
Ángel Piña <aps--@hotmail.com>
2022-06-13 08:22:12 +0200

    Add project page designs

--- src/components/project.js
+++ src/components/project.js
@@ -1,17 +1,31 @@
 import * as React from 'react';
-import { projectItem, header, body, title, description, tags } from './project.module.css';
+import './project.css';
 
 const Project = ({ project }) => {
   return (
-    <div className={projectItem}>
-      <div className={header}></div>
-      <div className={body}>
-        <div className={title}>{project.title}</div>
-        <div className={description}></div>
-        <div className={tags}></div>
+    <div className="project-item" onClick={onProjectClick}>
+      <div className="header">
+        <img src={project.projects.image1.mediaItemUrl} alt="thumbnail" />
+      </div>
+      <div className="body">
+        <div className="title"><a href={project.projects.url}>{project.title}</a></div>
+        <div className="description" dangerouslySetInnerHTML={{ __html: project.content }} />
+        <div className="tags">
+          {project.projects.tags.map((tag, index) => {
+            return (
+              <div key={index} className="tag">
+                {tag.name}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 };
+
+const onProjectClick = () => {
+
+}
 
 export default Project;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,9 +2,10 @@ import * as React from 'react';
 import Layout from '../components/layout';
 import Project from '../components/project';
 import { graphql, useStaticQuery } from 'gatsby';
-import { container, projectList } from './projects.module.css';
+import { container, projectList, search, header } from './projects.module.css';
 
 const ProjectsPage = (props) => {
+  const [searchProject, setSearch] = React.useState('');
   const data = useStaticQuery(graphql`
     query MyQuery {
       allWpPage {
@@ -37,15 +38,26 @@ const ProjectsPage = (props) => {
     }
   `);
 
+  const items = data.allWpPage.nodes.filter(
+    (node) => searchProject === '' || node.title.toLowerCase().search(searchProject.toLowerCase()) > -1
+  );
   return (
     <Layout pageTitle="Projects" {...props} max={true}>
       <section className={container}>
-        <h1>Projects</h1>
-        <p>Click on a project for details</p>
-        <input type="text" id="filter" name="filter" />
+        <h1 className={header}>Projects</h1>
+        <h3 className="info text-center">Click on a project for details</h3>
+        <input
+          type="text"
+          id="filter"
+          name="filter"
+          className={search}
+          placeholder="Search by project name"
+          value={searchProject}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <hr />
         <div className={projectList}>
-          {data.allWpPage.nodes.map((node, index) => {
+          {items.map((node, index) => {
             return <Project key={index} project={node} />;
           })}
         </div>
